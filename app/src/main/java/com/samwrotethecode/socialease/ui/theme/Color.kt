73e2a2746e14a7723a69b3bee8c5a1ad4f6c8dc9
samuel-wakoli <swwakoli@gmail.com>
2023-08-28 09:45:51 +0300

    added app colors

--- app/src/main/java/com/samwrotethecode/socialease/ui/theme/Color.kt
+++ app/src/main/java/com/samwrotethecode/socialease/ui/theme/Color.kt
@@ -2,10 +2,32 @@ package com.samwrotethecode.socialease.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val primaryLight = Color(color = 0xFF006399)
+val onPrimaryLight = Color(0xFFFFFFFF)
+val primaryContainerLight = Color(0xFFCDE5FF)
+val onPrimaryContainerLight = Color(0xFFCDE5FF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val secondaryLight = Color(color = 0xFFA04100)
+val onSecondaryLight = Color(0xFFFFFFFF)
+val secondaryContainerLight = Color(0xFFFFDBCC)
+val onSecondaryContainerLight = Color(0xFF351000)
+
+val tertiaryLight = Color(color = 0xFF006C47)
+val onTertiaryLight = Color(0xFFFFFFFF)
+val tertiaryContainerLight = Color(0xFF85F9BE)
+val onTertiaryContainerLight = Color(0xFF002113)
+
+val primaryDark = Color(color = 0xFF94CCFF)
+val onPrimaryDark = Color(0xFF003352)
+val primaryContainerDark = Color(0xFF004B74)
+val onPrimaryContainerDark = Color(0xFFCDE5FF)
+
+val secondaryDark = Color(color = 0xFFFFB693)
+val onSecondaryDark = Color(0xFF561F00)
+val secondaryContainerDark = Color(0xFF7A3000)
+val onSecondaryContainerDark = Color(0xFFFFDBCC)
+
+val tertiaryDark = Color(color = 0xFF68DCA4)
+val onTertiaryDark = Color(0xFF003823)
+val tertiaryContainerDark = Color(0xFF005234)
+val onTertiaryContainerDark = Color(0xFF85F9BE)
