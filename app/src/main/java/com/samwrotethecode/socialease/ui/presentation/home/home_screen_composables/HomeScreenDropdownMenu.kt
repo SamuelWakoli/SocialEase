d2cf26de4e8513cdf9fe113e4d253477e297815b
samuel-wakoli <swwakoli@gmail.com>
2023-10-01 19:54:03 +0300

    home screen dropdown / popup menu design

---
+++ app/src/main/java/com/samwrotethecode/socialease/ui/presentation/home/home_screen_composables/HomeScreenDropdownMenu.kt
@@ -0,0 +1,56 @@
+package com.samwrotethecode.socialease.ui.presentation.home.home_screen_composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.samwrotethecode.socialease.ui.presentation.home.viewmodels.HomeScreenViewModel
+import com.samwrotethecode.socialease.ui.presentation.home.viewmodels.HomeUiStateModel
+
+
+@Composable
+fun HomeScreenDropdownMenu(
+    uiState: HomeUiStateModel,
+    viewModel: HomeScreenViewModel,
+) {
+    DropdownMenu(
+        expanded = uiState.showDropdownMenu,
+        onDismissRequest = { viewModel.updateAppbarDropDownMenu() }
+    ) {
+        DropdownMenuItem(
+            text = {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    Icon(imageVector = Icons.Outlined.Settings, contentDescription = null)
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(text = "Settings")
+                }
+            },
+            onClick = { /*TODO*/ }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreenDropdownMenuPreview() {
+    HomeScreenDropdownMenu(
+        uiState = viewModel<HomeScreenViewModel>().uiState.collectAsState().value,
+        viewModel = viewModel<HomeScreenViewModel>(),
+    )
+}
