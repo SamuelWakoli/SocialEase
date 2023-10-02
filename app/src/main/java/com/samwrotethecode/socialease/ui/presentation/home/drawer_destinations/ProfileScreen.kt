bab945f5bb1b364602f7d8c8519ca20b86f70b0b
samuel-wakoli <swwakoli@gmail.com>
2023-10-02 19:20:24 +0300

    added ProfileScreen layout

--- app/src/main/java/com/samwrotethecode/socialease/ui/presentation/home/drawer_destinations/ProfileScreen.kt
+++ app/src/main/java/com/samwrotethecode/socialease/ui/presentation/home/drawer_destinations/ProfileScreen.kt
@@ -1,13 +1,46 @@
 package com.samwrotethecode.socialease.ui.presentation.home.drawer_destinations
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.samwrotethecode.socialease.ui.presentation.composables.CoilImage
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(navHostController: NavHostController) {
-
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "Profile")
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navHostController.navigateUp() }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Navigate back"
+                        )
+                    }
+                },
+            )
+        }
+    ) {
+        Column(modifier = Modifier.padding(it)) {
+            CoilImage()
+        }
+    }
 }
 
 @Preview
