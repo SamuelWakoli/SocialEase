171e30508c05a53894d57c5f9f865adbf2e8abd1
samuel-wakoli <swwakoli@gmail.com>
2023-10-05 21:40:53 +0300

    search screen design

---
+++ app/src/main/java/com/samwrotethecode/socialease/ui/presentation/home/appbar_destinations/SearchScreen.kt
@@ -0,0 +1,75 @@
+package com.samwrotethecode.socialease.ui.presentation.home.appbar_destinations
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun SearchScreen(navHostController: NavHostController) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Card(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            TextField(
+                value = "",
+                onValueChange = {},
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                ),
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterHorizontally)
+                    .widthIn(max = 600.dp, min = 420.dp),
+                leadingIcon = {
+                    IconButton(onClick = { navHostController.navigateUp() }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Navigate back"
+                        )
+                    }
+                },
+                placeholder = {
+                    Text(text = "Search")
+                },
+                trailingIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Outlined.Search, contentDescription = "Search")
+                    }
+                }
+            )
+        }
+    }
+
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+)
+@Composable
+fun SearchScreenPreview() {
+    SearchScreen(navHostController = rememberNavController())
+}
