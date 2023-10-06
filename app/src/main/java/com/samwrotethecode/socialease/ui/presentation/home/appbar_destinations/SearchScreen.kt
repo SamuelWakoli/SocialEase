72c9cba6788fe04304321be0f61bcf96b42f4695
samuel-wakoli <swwakoli@gmail.com>
2023-10-06 09:36:23 +0300

    refine search screen

--- app/src/main/java/com/samwrotethecode/socialease/ui/presentation/home/appbar_destinations/SearchScreen.kt
+++ app/src/main/java/com/samwrotethecode/socialease/ui/presentation/home/appbar_destinations/SearchScreen.kt
@@ -1,18 +1,23 @@
 package com.samwrotethecode.socialease.ui.presentation.home.appbar_destinations
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.SearchOff
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -20,6 +25,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -27,8 +33,12 @@ import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun SearchScreen(navHostController: NavHostController) {
+
+    var noResults: Boolean = true
+
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Card(
             modifier = Modifier.padding(16.dp)
@@ -60,6 +70,34 @@ fun SearchScreen(navHostController: NavHostController) {
                     }
                 }
             )
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement =
+            if (noResults) Arrangement.Center else Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            if (noResults) {
+                Icon(
+                    imageVector = Icons.Outlined.SearchOff,
+                    contentDescription = null,
+                    modifier = Modifier.size(64.dp),
+                    tint = MaterialTheme.colorScheme.primary,
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    text = "No results found",
+                    style = TextStyle(
+                        fontSize = MaterialTheme.typography.headlineSmall.fontSize,
+                        fontWeight = MaterialTheme.typography.headlineSmall.fontWeight,
+                        color = MaterialTheme.colorScheme.primary
+                    ),
+                )
+            } else {
+                // TODO: return search results
+            }
         }
     }
 
