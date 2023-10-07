ee95d2b3086cd94dbb2b4a3e8dbb2098327dac08
samuel-wakoli <swwakoli@gmail.com>
2023-10-07 23:24:01 +0300

    subtopic list item design

---
+++ app/src/main/java/com/samwrotethecode/socialease/ui/presentation/home/content/SubTopicListItem.kt
@@ -0,0 +1,99 @@
+package com.samwrotethecode.socialease.ui.presentation.home.content
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.BookmarkAdd
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.samwrotethecode.socialease.R
+
+@Composable
+fun SubTopicListItem(
+    title: String,
+    description: String,
+    onClick: () -> Unit = {},
+) {
+    Card(
+        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+            contentColor = MaterialTheme.colorScheme.onTertiaryContainer,
+        )
+    ) {
+        ListItem(
+            headlineContent = {
+                Text(
+                    text = title,
+                    color = MaterialTheme.colorScheme.onTertiaryContainer
+                )
+            },
+            supportingContent = {
+                Column {
+                    Text(
+                        text = buildAnnotatedString {
+                            withStyle(SpanStyle(color = MaterialTheme.colorScheme.onTertiaryContainer)) {
+                                append(description)
+                            }
+                            append(" ")
+                            withStyle(SpanStyle(color = MaterialTheme.colorScheme.secondary)) {
+                                append("read more")
+                            }
+                        },
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                    Row(
+                        horizontalArrangement = Arrangement.End,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth(),
+                    ) {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.Outlined.BookmarkAdd,
+                                contentDescription = "Add to bookmarks",
+                                tint = MaterialTheme.colorScheme.primary
+                            )
+                        }
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.Outlined.Share,
+                                contentDescription = "Share",
+                                tint = MaterialTheme.colorScheme.primary
+                            )
+                        }
+                    }
+                }
+            }
+        )
+    }
+}
+
+@Preview(showSystemUi = false, showBackground = true)
+@Composable
+fun SubTopicListItemPreview() {
+    MaterialTheme {
+        SubTopicListItem(
+            title = "Title",
+            description = stringResource(id = R.string.lorem_ipsum)
+        )
+    }
+}
