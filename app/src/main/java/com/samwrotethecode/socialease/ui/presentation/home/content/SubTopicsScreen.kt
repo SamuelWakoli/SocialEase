dd632cf4072c1289c410e9ca58d450d96efda8e0
sam <swwakoli@gmail.com>
2023-10-13 08:46:57 +0300

    - navigates to ReadingScreen by updating its state - added background image

--- app/src/main/java/com/samwrotethecode/socialease/ui/presentation/home/content/SubTopicsScreen.kt
+++ app/src/main/java/com/samwrotethecode/socialease/ui/presentation/home/content/SubTopicsScreen.kt
@@ -1,6 +1,8 @@
 package com.samwrotethecode.socialease.ui.presentation.home.content
 
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -15,16 +17,23 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.samwrotethecode.socialease.R
+import com.samwrotethecode.socialease.data.local_data.SubTopicsModel
 import com.samwrotethecode.socialease.ui.presentation.home.viewmodels.HomeScreenViewModel
+import com.samwrotethecode.socialease.ui.presentation.navigation.Screens
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -35,33 +44,60 @@ fun SubTopicsScreen(
 
     val uiState = viewModel.uiState.collectAsState().value
 
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Log.d(
-                        "CURRENT SUB TOPIC TITLE ID",
-                        "ACCESSING: ${uiState.currentSubTopicTitleId} "
-                    )
-                    Text(text = stringResource(id = uiState.currentSubTopicTitleId!!))
-                },
-                navigationIcon = {
-                    IconButton(onClick = { navHostController.navigateUp() }) {
-                        Icon(
-                            imageVector = Icons.Default.ArrowBack,
-                            contentDescription = "Navigate back"
+    Box {
+        Image(
+            painter = painterResource(id = uiState.currentBackgroundImage!!),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop,
+            alpha = 0.2f
+        )
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            containerColor = Color.Transparent,
+            contentColor = Color.Transparent,
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Log.d(
+                            "CURRENT SUB TOPIC TITLE ID",
+                            "ACCESSING: ${uiState.currentSubTopicTitleId} "
                         )
-                    }
-                },
-            )
-        }
-    ) {
-        LazyColumn(contentPadding = it) {
-            items(uiState.currentSubTopicsList!!.size) { index ->
-                SubTopicListItem(
-                    title = stringResource(id = uiState.currentSubTopicsList[index].titleId),
-                    generalDescription = stringResource(id = uiState.currentSubTopicsList[index].generalDescriptionId)
+                        Text(text = stringResource(id = uiState.currentSubTopicTitleId!!))
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = { navHostController.navigateUp() }) {
+                            Icon(
+                                imageVector = Icons.Default.ArrowBack,
+                                contentDescription = "Navigate back"
+                            )
+                        }
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = Color.Transparent,
+                    ),
                 )
+            }
+        ) {
+            LazyColumn(contentPadding = it) {
+                items(uiState.currentSubTopicsList!!.size) { index ->
+                    SubTopicListItem(
+                        title = stringResource(id = uiState.currentSubTopicsList[index].titleId),
+                        generalDescription = stringResource(id = uiState.currentSubTopicsList[index].generalDescriptionId),
+                        onClick = {
+                            navHostController.navigate(Screens.ReadingScreen.route) {
+                                launchSingleTop = true
+                                viewModel.updateReadingScreenState(
+                                    SubTopicsModel(
+                                        titleId = uiState.currentSubTopicsList[index].titleId,
+                                        generalDescriptionId = uiState.currentSubTopicsList[index].generalDescriptionId,
+                                        content = uiState.currentSubTopicsList[index].content,
+                                    )
+                                )
+                            }
+                        }
+                    )
+                }
             }
         }
     }
