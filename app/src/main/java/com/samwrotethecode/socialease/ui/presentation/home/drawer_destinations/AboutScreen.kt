bd647eb395d168a9fb2888abb184e63b1011f248
sam <swwakoli@gmail.com>
2023-10-13 15:15:43 +0300

    fixed a few bugs in AboutScreen

--- app/src/main/java/com/samwrotethecode/socialease/ui/presentation/home/drawer_destinations/AboutScreen.kt
+++ app/src/main/java/com/samwrotethecode/socialease/ui/presentation/home/drawer_destinations/AboutScreen.kt
@@ -1,9 +1,13 @@
 package com.samwrotethecode.socialease.ui.presentation.home.drawer_destinations
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -13,13 +17,22 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.samwrotethecode.socialease.R
@@ -27,56 +40,79 @@ import com.samwrotethecode.socialease.R
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AboutScreen(navHostController: NavHostController) {
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                navigationIcon = {
-                    IconButton(onClick = { navHostController.navigateUp() }) {
-                        Icon(
-                            imageVector = Icons.Default.ArrowBack,
-                            contentDescription = stringResource(R.string.navigate_back)
-                        )
-                    }
-                },
-                title = {
-                    Text(text = stringResource(id = R.string.app_name))
-                },
-            )
-        },
-    ) { paddingValues ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-                .verticalScroll(rememberScrollState()),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-        ) {
-            ListItem(
-                headlineContent = { },
-                supportingContent = {
-                    Text(
-                        text = stringResource(R.string.about_social_skills)
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.intro_img_1),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop,
+            alpha = 0.2f
+        )
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    navigationIcon = {
+                        IconButton(onClick = { navHostController.navigateUp() }) {
+                            Icon(
+                                imageVector = Icons.Default.ArrowBack,
+                                contentDescription = stringResource(R.string.navigate_back)
+                            )
+                        }
+                    },
+                    title = {
+                        Text(text = stringResource(id = R.string.app_name))
+                    },
+                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                        containerColor = Color.Transparent
                     )
-                },
-            )
-            ListItem(
-                headlineContent = { Text(text = stringResource(R.string.why_are_social_skills_important)) },
-                supportingContent = {
-                    Text(
-                        text = stringResource(R.string.why_are_social_skills_important_description)
+                )
+            },
+            containerColor = Color.Transparent
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .background(Color.Transparent)
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Column(
+                    modifier = Modifier
+                        .widthIn(max = 800.dp)
+                        .clip(MaterialTheme.shapes.medium)
+                        .padding(16.dp)
+                        .background(Color.Transparent)
+                ) {
+                    ListItem(
+                        headlineContent = { },
+                        supportingContent = {
+                            Text(
+                                text = stringResource(R.string.about_social_skills)
+                            )
+                        },
                     )
-                },
-            )
-            ListItem(
-                headlineContent = { Text(text = stringResource(R.string.how_to_improve_your_social_skills)) },
-                supportingContent = {
-                    Text(
-                        text = stringResource(R.string.how_to_improve_your_social_skills_description)
+                    ListItem(
+                        headlineContent = { Text(text = stringResource(R.string.why_are_social_skills_important)) },
+                        supportingContent = {
+                            Text(
+                                text = stringResource(R.string.why_are_social_skills_important_description)
+                            )
+                        },
                     )
-                },
-            )
-
+                    ListItem(
+                        headlineContent = { Text(text = stringResource(R.string.how_to_improve_your_social_skills)) },
+                        supportingContent = {
+                            Text(
+                                text = stringResource(R.string.how_to_improve_your_social_skills_description)
+                            )
+                        },
+                    )
+                }
+            }
         }
     }
 }
