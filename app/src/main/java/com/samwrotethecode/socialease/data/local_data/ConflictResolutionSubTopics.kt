a35b1b92c55a23fee62120cbf809e502010dd71d
sam <swwakoli@gmail.com>
2023-10-14 19:25:50 +0300

    added conflictResolutionSubTopics to the app

---
+++ app/src/main/java/com/samwrotethecode/socialease/data/local_data/ConflictResolutionSubTopics.kt
@@ -0,0 +1,120 @@
+package com.samwrotethecode.socialease.data.local_data
+
+import com.samwrotethecode.socialease.R
+
+var conflictResolutionSubTopics = listOf<SubTopicsModel>(
+    SubTopicsModel(
+        titleId = R.string.identifying_conflict,
+        generalDescriptionId = R.string.identifying_conflict_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.identifying_conflict_content_1,
+                descriptionId = R.string.identifying_conflict_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.identifying_conflict_content_2,
+                descriptionId = R.string.identifying_conflict_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.identifying_conflict_content_3,
+                descriptionId = R.string.identifying_conflict_content_description_3,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.identifying_conflict_content_4,
+                descriptionId = R.string.identifying_conflict_content_description_4,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.identifying_conflict_conclusion,
+                descriptionId = R.string.identifying_conflict_conclusion_description,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.active_listening_in_conflicts,
+        generalDescriptionId = R.string.active_listening_in_conflicts_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.active_listening_in_conflicts_content_1,
+                descriptionId = R.string.active_listening_in_conflicts_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.active_listening_in_conflicts_content_2,
+                descriptionId = R.string.active_listening_in_conflicts_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.active_listening_in_conflicts_content_3,
+                descriptionId = R.string.active_listening_in_conflicts_content_description_3,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.active_listening_in_conflicts_content_4,
+                descriptionId = R.string.active_listening_in_conflicts_content_description_4,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.mediation,
+        generalDescriptionId = R.string.mediation_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.mediation_content_1,
+                descriptionId = R.string.mediation_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.mediation_content_2,
+                descriptionId = R.string.mediation_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.mediation_content_3,
+                descriptionId = R.string.mediation_content_description_3,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.mediation_content_4,
+                descriptionId = R.string.mediation_content_description_4,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.conflict_de_escalation,
+        generalDescriptionId = R.string.conflict_de_escalation_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.conflict_de_escalation_content_1,
+                descriptionId = R.string.conflict_de_escalation_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.conflict_de_escalation_content_2,
+                descriptionId = R.string.conflict_de_escalation_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.conflict_de_escalation_content_3,
+                descriptionId = R.string.conflict_de_escalation_content_description_3,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.conflict_de_escalation_content_4,
+                descriptionId = R.string.conflict_de_escalation_content_description_4,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.finding_common_ground,
+        generalDescriptionId = R.string.finding_common_ground_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.finding_common_ground_content_1,
+                descriptionId = R.string.finding_common_ground_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.finding_common_ground_content_2,
+                descriptionId = R.string.finding_common_ground_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.finding_common_ground_content_3,
+                descriptionId = R.string.finding_common_ground_content_description_3,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.finding_common_ground_content_4,
+                descriptionId = R.string.finding_common_ground_content_description_4,
+            )
+        )
+    ),
+)
