85b8a7ca0d38b4512ffa5524ca276e4c85f2bc2f
sam <swwakoli@gmail.com>
2023-10-14 18:39:00 +0300

    added cooperationSubTopics to the app

---
+++ app/src/main/java/com/samwrotethecode/socialease/data/local_data/CooperationSubTopics.kt
@@ -0,0 +1,163 @@
+package com.samwrotethecode.socialease.data.local_data
+
+import com.samwrotethecode.socialease.R
+
+var cooperationSubTopics = listOf<SubTopicsModel>(
+    SubTopicsModel(
+        titleId = R.string.sharing,
+        generalDescriptionId = R.string.sharing_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.sharing_content_1,
+                descriptionId = R.string.sharing_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.sharing_content_2,
+                descriptionId = R.string.sharing_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.sharing_content_3,
+                descriptionId = R.string.sharing_content_description_3,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.sharing_content_4,
+                descriptionId = R.string.sharing_content_description_4,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.sharing_conclusion,
+                descriptionId = R.string.sharing_conclusion_description,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.compromise,
+        generalDescriptionId = R.string.compromise_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.compromise_content_1,
+                descriptionId = R.string.compromise_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.compromise_content_2,
+                descriptionId = R.string.compromise_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.compromise_content_3,
+                descriptionId = R.string.compromise_content_description_3,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.compromise_content_4,
+                descriptionId = R.string.compromise_content_description_4,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.compromise_conclusion,
+                descriptionId = R.string.compromise_conclusion_description,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.teamwork,
+        generalDescriptionId = R.string.teamwork_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.teamwork_content_1,
+                descriptionId = R.string.teamwork_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.teamwork_content_2,
+                descriptionId = R.string.teamwork_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.teamwork_content_3,
+                descriptionId = R.string.teamwork_content_description_3,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.teamwork_content_4,
+                descriptionId = R.string.teamwork_content_description_4,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.teamwork_conclusion,
+                descriptionId = R.string.teamwork_conclusion_description,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.collaboration,
+        generalDescriptionId = R.string.collaboration_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.collaboration_content_1,
+                descriptionId = R.string.collaboration_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.collaboration_content_2,
+                descriptionId = R.string.collaboration_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.collaboration_content_3,
+                descriptionId = R.string.collaboration_content_description_3,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.collaboration_content_4,
+                descriptionId = R.string.collaboration_content_description_4,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.collaboration_conclusion,
+                descriptionId = R.string.collaboration_conclusion_description,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.supporting_others,
+        generalDescriptionId = R.string.supporting_others_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.supporting_others_content_1,
+                descriptionId = R.string.supporting_others_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.supporting_others_content_2,
+                descriptionId = R.string.supporting_others_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.supporting_others_content_3,
+                descriptionId = R.string.supporting_others_content_description_3,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.supporting_others_content_4,
+                descriptionId = R.string.supporting_others_content_description_4,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.supporting_others_conclusion,
+                descriptionId = R.string.supporting_others_conclusion_description,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.group_decision_making,
+        generalDescriptionId = R.string.group_decision_making_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.group_decision_making_content_1,
+                descriptionId = R.string.group_decision_making_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.group_decision_making_content_2,
+                descriptionId = R.string.group_decision_making_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.group_decision_making_content_3,
+                descriptionId = R.string.group_decision_making_content_description_3,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.group_decision_making_content_4,
+                descriptionId = R.string.group_decision_making_content_description_4,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.group_decision_making_conclusion,
+                descriptionId = R.string.group_decision_making_conclusion_description,
+            )
+        )
+    ),
+
+)
