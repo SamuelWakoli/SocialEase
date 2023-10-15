666de936de5a9b5b1ed953d79716631de535ca81
sam <swwakoli@gmail.com>
2023-10-15 15:06:22 +0300

    added selfManagementSubTopics to the app

---
+++ app/src/main/java/com/samwrotethecode/socialease/data/local_data/SelfManagementSubTopics.kt
@@ -0,0 +1,116 @@
+package com.samwrotethecode.socialease.data.local_data
+
+import com.samwrotethecode.socialease.R
+
+var selfManagementSubTopics = listOf<SubTopicsModel>(
+    SubTopicsModel(
+        titleId = R.string.emotional_regulation,
+        generalDescriptionId = R.string.emotional_regulation_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.emotional_regulation_content_1,
+                descriptionId = R.string.emotional_regulation_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.emotional_regulation_content_2,
+                descriptionId = R.string.emotional_regulation_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.emotional_regulation_content_3,
+                descriptionId = R.string.emotional_regulation_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.stress_management,
+        generalDescriptionId = R.string.stress_management_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.stress_management_content_1,
+                descriptionId = R.string.stress_management_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.stress_management_content_2,
+                descriptionId = R.string.stress_management_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.stress_management_content_3,
+                descriptionId = R.string.stress_management_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.time_management,
+        generalDescriptionId = R.string.time_management_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.time_management_content_1,
+                descriptionId = R.string.time_management_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.time_management_content_2,
+                descriptionId = R.string.time_management_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.time_management_content_3,
+                descriptionId = R.string.time_management_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.goal_setting,
+        generalDescriptionId = R.string.goal_setting_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.goal_setting_content_1,
+                descriptionId = R.string.goal_setting_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.goal_setting_content_2,
+                descriptionId = R.string.goal_setting_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.goal_setting_content_3,
+                descriptionId = R.string.goal_setting_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.self_care,
+        generalDescriptionId = R.string.self_care_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.self_care_content_1,
+                descriptionId = R.string.self_care_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.self_care_content_2,
+                descriptionId = R.string.self_care_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.self_care_content_3,
+                descriptionId = R.string.self_care_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.handling_pressure,
+        generalDescriptionId = R.string.handling_pressure_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.handling_pressure_content_1,
+                descriptionId = R.string.handling_pressure_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.handling_pressure_content_2,
+                descriptionId = R.string.handling_pressure_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.handling_pressure_content_3,
+                descriptionId = R.string.handling_pressure_content_description_3,
+            )
+        )
+    ),
+
+
+    )
