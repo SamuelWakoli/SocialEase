6311990f1fdf16666b8b004028e49855f4a2be8f
sam <swwakoli@gmail.com>
2023-10-15 10:26:24 +0300

    added problemSolvingSubTopics to the app

---
+++ app/src/main/java/com/samwrotethecode/socialease/data/local_data/ProblemSolvingSubTopics.kt
@@ -0,0 +1,127 @@
+package com.samwrotethecode.socialease.data.local_data
+
+import com.samwrotethecode.socialease.R
+
+var problemSolvingSubTopics = listOf(
+    SubTopicsModel(
+        titleId = R.string.problem_identification,
+        generalDescriptionId = R.string.problem_identification_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.problem_identification_content_1,
+                descriptionId = R.string.problem_identification_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.problem_identification_content_2,
+                descriptionId = R.string.problem_identification_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.problem_identification_content_3,
+                descriptionId = R.string.problem_identification_content_description_3,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.problem_identification_content_4,
+                descriptionId = R.string.problem_identification_content_description_4,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.critical_thinking,
+        generalDescriptionId = R.string.critical_thinking_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.critical_thinking_content_1,
+                descriptionId = R.string.critical_thinking_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.critical_thinking_content_2,
+                descriptionId = R.string.critical_thinking_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.critical_thinking_content_3,
+                descriptionId = R.string.critical_thinking_content_description_3,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.critical_thinking_content_4,
+                descriptionId = R.string.critical_thinking_content_description_4,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.analyzing_options,
+        generalDescriptionId = R.string.analyzing_options_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.analyzing_options_content_1,
+                descriptionId = R.string.analyzing_options_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.analyzing_options_content_2,
+                descriptionId = R.string.analyzing_options_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.analyzing_options_content_3,
+                descriptionId = R.string.analyzing_options_content_description_3,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.analyzing_options_content_4,
+                descriptionId = R.string.analyzing_options_content_description_4,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.decision_making,
+        generalDescriptionId = R.string.decision_making_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.decision_making_content_1,
+                descriptionId = R.string.decision_making_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.decision_making_content_2,
+                descriptionId = R.string.decision_making_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.decision_making_content_3,
+                descriptionId = R.string.decision_making_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.implementation_of_solutions,
+        generalDescriptionId = R.string.implementation_of_solutions_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.implementation_of_solutions_content_1,
+                descriptionId = R.string.implementation_of_solutions_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.implementation_of_solutions_content_2,
+                descriptionId = R.string.implementation_of_solutions_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.implementation_of_solutions_content_3,
+                descriptionId = R.string.implementation_of_solutions_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.evaluation_of_outcomes,
+        generalDescriptionId = R.string.evaluation_of_outcomes_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.evaluation_of_outcomes_content_1,
+                descriptionId = R.string.evaluation_of_outcomes_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.evaluation_of_outcomes_content_2,
+                descriptionId = R.string.evaluation_of_outcomes_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.evaluation_of_outcomes_content_3,
+                descriptionId = R.string.evaluation_of_outcomes_content_description_3,
+            )
+        )
+    ),
+
+    )
