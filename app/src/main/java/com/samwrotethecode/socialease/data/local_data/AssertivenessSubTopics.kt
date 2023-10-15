73412d6d7ecedf6ccaebc0437e6cdcb2b0660284
sam <swwakoli@gmail.com>
2023-10-15 15:58:57 +0300

    added assertivenessSubTopics to the app

---
+++ app/src/main/java/com/samwrotethecode/socialease/data/local_data/AssertivenessSubTopics.kt
@@ -0,0 +1,104 @@
+package com.samwrotethecode.socialease.data.local_data
+
+import com.samwrotethecode.socialease.R
+
+var assertivenessSubTopics = listOf<SubTopicsModel>(
+    SubTopicsModel(
+        titleId = R.string.expressing_needs_clearly,
+        generalDescriptionId = R.string.expressing_needs_clearly_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.expressing_needs_clearly_content_1,
+                descriptionId = R.string.expressing_needs_clearly_content_description_1,
+            ), SubTopicContentItemModel(
+                titleId = R.string.expressing_needs_clearly_content_2,
+                descriptionId = R.string.expressing_needs_clearly_content_description_2,
+            ), SubTopicContentItemModel(
+                titleId = R.string.expressing_needs_clearly_content_3,
+                descriptionId = R.string.expressing_needs_clearly_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.setting_boundaries,
+        generalDescriptionId = R.string.setting_boundaries_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.setting_boundaries_content_1,
+                descriptionId = R.string.setting_boundaries_content_description_1,
+            ), SubTopicContentItemModel(
+                titleId = R.string.setting_boundaries_content_2,
+                descriptionId = R.string.setting_boundaries_content_description_2,
+            ), SubTopicContentItemModel(
+                titleId = R.string.setting_boundaries_content_3,
+                descriptionId = R.string.setting_boundaries_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.conflict_resolution_with_assertiveness,
+        generalDescriptionId = R.string.conflict_resolution_with_assertiveness_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.conflict_resolution_with_assertiveness_content_1,
+                descriptionId = R.string.conflict_resolution_with_assertiveness_content_description_1,
+            ), SubTopicContentItemModel(
+                titleId = R.string.conflict_resolution_with_assertiveness_content_2,
+                descriptionId = R.string.conflict_resolution_with_assertiveness_content_description_2,
+            ), SubTopicContentItemModel(
+                titleId = R.string.conflict_resolution_with_assertiveness_content_3,
+                descriptionId = R.string.conflict_resolution_with_assertiveness_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.respecting_others_rights,
+        generalDescriptionId = R.string.respecting_others_rights_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.respecting_others_rights_content_1,
+                descriptionId = R.string.respecting_others_rights_content_description_1,
+            ), SubTopicContentItemModel(
+                titleId = R.string.respecting_others_rights_content_2,
+                descriptionId = R.string.respecting_others_rights_content_description_2,
+            ), SubTopicContentItemModel(
+                titleId = R.string.respecting_others_rights_content_3,
+                descriptionId = R.string.respecting_others_rights_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.self_advocacy,
+        generalDescriptionId = R.string.self_advocacy_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.self_advocacy_content_1,
+                descriptionId = R.string.self_advocacy_content_description_1,
+            ), SubTopicContentItemModel(
+                titleId = R.string.self_advocacy_content_2,
+                descriptionId = R.string.self_advocacy_content_description_2,
+            ), SubTopicContentItemModel(
+                titleId = R.string.self_advocacy_content_3,
+                descriptionId = R.string.self_advocacy_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.confidence_building,
+        generalDescriptionId = R.string.confidence_building_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.confidence_building_content_1,
+                descriptionId = R.string.confidence_building_content_description_1,
+            ), SubTopicContentItemModel(
+                titleId = R.string.confidence_building_content_2,
+                descriptionId = R.string.confidence_building_content_description_2,
+            ), SubTopicContentItemModel(
+                titleId = R.string.confidence_building_content_3,
+                descriptionId = R.string.confidence_building_content_description_3,
+            )
+        )
+    ),
+
+
+    )
