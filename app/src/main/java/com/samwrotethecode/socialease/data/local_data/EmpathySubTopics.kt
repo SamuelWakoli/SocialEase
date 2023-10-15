29c99100628eb803a519650b6259fc634858aaee
sam <swwakoli@gmail.com>
2023-10-15 15:22:37 +0300

    added empathySubTopics to the app

---
+++ app/src/main/java/com/samwrotethecode/socialease/data/local_data/EmpathySubTopics.kt
@@ -0,0 +1,104 @@
+package com.samwrotethecode.socialease.data.local_data
+
+import com.samwrotethecode.socialease.R
+
+var empathySubTopics = listOf<SubTopicsModel>(
+    SubTopicsModel(
+        titleId = R.string.cognitive_empathy,
+        generalDescriptionId = R.string.cognitive_empathy_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.cognitive_empathy_content_1,
+                descriptionId = R.string.cognitive_empathy_content_description_1,
+            ), SubTopicContentItemModel(
+                titleId = R.string.cognitive_empathy_content_2,
+                descriptionId = R.string.cognitive_empathy_content_description_2,
+            ), SubTopicContentItemModel(
+                titleId = R.string.cognitive_empathy_content_3,
+                descriptionId = R.string.cognitive_empathy_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.emotional_empathy,
+        generalDescriptionId = R.string.emotional_empathy_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.emotional_empathy_content_1,
+                descriptionId = R.string.emotional_empathy_content_description_1,
+            ), SubTopicContentItemModel(
+                titleId = R.string.emotional_empathy_content_2,
+                descriptionId = R.string.emotional_empathy_content_description_2,
+            ), SubTopicContentItemModel(
+                titleId = R.string.emotional_empathy_content_3,
+                descriptionId = R.string.emotional_empathy_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.compassion,
+        generalDescriptionId = R.string.compassion_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.compassion_content_1,
+                descriptionId = R.string.compassion_content_description_1,
+            ), SubTopicContentItemModel(
+                titleId = R.string.compassion_content_2,
+                descriptionId = R.string.compassion_content_description_2,
+            ), SubTopicContentItemModel(
+                titleId = R.string.compassion_content_3,
+                descriptionId = R.string.compassion_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.empathetic_communication,
+        generalDescriptionId = R.string.empathetic_communication_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.empathetic_communication_content_1,
+                descriptionId = R.string.empathetic_communication_content_description_1,
+            ), SubTopicContentItemModel(
+                titleId = R.string.empathetic_communication_content_2,
+                descriptionId = R.string.empathetic_communication_content_description_2,
+            ), SubTopicContentItemModel(
+                titleId = R.string.empathetic_communication_content_3,
+                descriptionId = R.string.empathetic_communication_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.empathetic_listening,
+        generalDescriptionId = R.string.empathetic_listening_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.empathetic_listening_content_1,
+                descriptionId = R.string.empathetic_listening_content_description_1,
+            ), SubTopicContentItemModel(
+                titleId = R.string.empathetic_listening_content_2,
+                descriptionId = R.string.empathetic_listening_content_description_2,
+            ), SubTopicContentItemModel(
+                titleId = R.string.empathetic_listening_content_3,
+                descriptionId = R.string.empathetic_listening_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.responding_to_others_needs,
+        generalDescriptionId = R.string.responding_to_others_needs_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.responding_to_others_needs_content_1,
+                descriptionId = R.string.responding_to_others_needs_content_description_1,
+            ), SubTopicContentItemModel(
+                titleId = R.string.responding_to_others_needs_content_2,
+                descriptionId = R.string.responding_to_others_needs_content_description_2,
+            ), SubTopicContentItemModel(
+                titleId = R.string.responding_to_others_needs_content_3,
+                descriptionId = R.string.responding_to_others_needs_content_description_3,
+            )
+        )
+    ),
+
+
+    )
