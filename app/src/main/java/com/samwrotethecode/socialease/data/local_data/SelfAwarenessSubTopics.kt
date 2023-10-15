4395b68144176a62d4544cc7f03d1941970d1ae8
sam <swwakoli@gmail.com>
2023-10-15 14:38:08 +0300

    added selfAwarenessSubTopics to the app

---
+++ app/src/main/java/com/samwrotethecode/socialease/data/local_data/SelfAwarenessSubTopics.kt
@@ -0,0 +1,115 @@
+package com.samwrotethecode.socialease.data.local_data
+
+import com.samwrotethecode.socialease.R
+
+var selfAwarenessSubTopics = listOf<SubTopicsModel>(
+    SubTopicsModel(
+        titleId = R.string.self_reflection,
+        generalDescriptionId = R.string.self_reflection_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.self_reflection_content_1,
+                descriptionId = R.string.self_reflection_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.self_reflection_content_2,
+                descriptionId = R.string.self_reflection_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.self_reflection_content_3,
+                descriptionId = R.string.self_reflection_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.recognizing_emotions,
+        generalDescriptionId = R.string.recognizing_emotions_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.recognizing_emotions_content_1,
+                descriptionId = R.string.recognizing_emotions_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.recognizing_emotions_content_2,
+                descriptionId = R.string.recognizing_emotions_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.recognizing_emotions_content_3,
+                descriptionId = R.string.recognizing_emotions_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.understanding_personal_values,
+        generalDescriptionId = R.string.understanding_personal_values_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.understanding_personal_values_content_1,
+                descriptionId = R.string.understanding_personal_values_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.understanding_personal_values_content_2,
+                descriptionId = R.string.understanding_personal_values_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.understanding_personal_values_content_3,
+                descriptionId = R.string.understanding_personal_values_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.self_identity,
+        generalDescriptionId = R.string.self_identity_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.self_identity_content_1,
+                descriptionId = R.string.self_identity_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.self_identity_content_2,
+                descriptionId = R.string.self_identity_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.self_identity_content_3,
+                descriptionId = R.string.self_identity_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.self_improvement,
+        generalDescriptionId = R.string.self_improvement_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.self_improvement_content_1,
+                descriptionId = R.string.self_improvement_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.self_improvement_content_2,
+                descriptionId = R.string.self_improvement_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.self_improvement_content_3,
+                descriptionId = R.string.self_improvement_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.self_regulation,
+        generalDescriptionId = R.string.self_regulation_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.self_regulation_content_1,
+                descriptionId = R.string.self_regulation_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.self_regulation_content_2,
+                descriptionId = R.string.self_regulation_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.self_regulation_content_3,
+                descriptionId = R.string.self_regulation_content_description_3,
+            )
+        )
+    ),
+
+)
