25cb83cbb64991e3de92c39dcf4d496477978ad3
sam <swwakoli@gmail.com>
2023-10-15 14:55:26 +0300

    added relationshipBuildingSubTopics to the app

---
+++ app/src/main/java/com/samwrotethecode/socialease/data/local_data/RelationshipBuildingSubTopics.kt
@@ -0,0 +1,107 @@
+package com.samwrotethecode.socialease.data.local_data
+
+import com.samwrotethecode.socialease.R
+
+var relationshipBuildingSubTopics = listOf<SubTopicsModel>(
+    SubTopicsModel(
+        titleId = R.string.building_trust,
+        generalDescriptionId = R.string.building_trust_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.building_trust_content_1,
+                descriptionId = R.string.building_trust_content_description_1,
+            ), SubTopicContentItemModel(
+                titleId = R.string.building_trust_content_2,
+                descriptionId = R.string.building_trust_content_description_2,
+            ), SubTopicContentItemModel(
+                titleId = R.string.building_trust_content_3,
+                descriptionId = R.string.building_trust_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.building_rapport,
+        generalDescriptionId = R.string.building_rapport_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.building_rapport_content_1,
+                descriptionId = R.string.building_rapport_content_description_1,
+            ), SubTopicContentItemModel(
+                titleId = R.string.building_rapport_content_2,
+                descriptionId = R.string.building_rapport_content_description_2,
+            ), SubTopicContentItemModel(
+                titleId = R.string.building_rapport_content_3,
+                descriptionId = R.string.building_rapport_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.active_listening_in_relationships,
+        generalDescriptionId = R.string.active_listening_in_relationships_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.active_listening_in_relationships_content_1,
+                descriptionId = R.string.active_listening_in_relationships_content_description_1,
+            ), SubTopicContentItemModel(
+                titleId = R.string.active_listening_in_relationships_content_2,
+                descriptionId = R.string.active_listening_in_relationships_content_description_2,
+            ), SubTopicContentItemModel(
+                titleId = R.string.active_listening_in_relationships_content_3,
+                descriptionId = R.string.active_listening_in_relationships_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.empathy_in_relationships,
+        generalDescriptionId = R.string.empathy_in_relationships_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.empathy_in_relationships_content_1,
+                descriptionId = R.string.empathy_in_relationships_content_description_1,
+            ), SubTopicContentItemModel(
+                titleId = R.string.empathy_in_relationships_content_2,
+                descriptionId = R.string.empathy_in_relationships_content_description_2,
+            ), SubTopicContentItemModel(
+                titleId = R.string.empathy_in_relationships_content_3,
+                descriptionId = R.string.empathy_in_relationships_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.conflict_resolution_in_relationships,
+        generalDescriptionId = R.string.conflict_resolution_in_relationships_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.conflict_resolution_in_relationships_content_1,
+                descriptionId = R.string.conflict_resolution_in_relationships_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.conflict_resolution_in_relationships_content_2,
+                descriptionId = R.string.conflict_resolution_in_relationships_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.conflict_resolution_in_relationships_content_3,
+                descriptionId = R.string.conflict_resolution_in_relationships_content_description_3,
+            )
+        )
+    ),
+    SubTopicsModel(
+        titleId = R.string.supporting_friends_and_partners,
+        generalDescriptionId = R.string.supporting_friends_and_partners_general_description,
+        content = listOf(
+            SubTopicContentItemModel(
+                titleId = R.string.supporting_friends_and_partners_content_1,
+                descriptionId = R.string.supporting_friends_and_partners_content_description_1,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.supporting_friends_and_partners_content_2,
+                descriptionId = R.string.supporting_friends_and_partners_content_description_2,
+            ),
+            SubTopicContentItemModel(
+                titleId = R.string.supporting_friends_and_partners_content_3,
+                descriptionId = R.string.supporting_friends_and_partners_content_description_3,
+            )
+        )
+    ),
+
+    )
