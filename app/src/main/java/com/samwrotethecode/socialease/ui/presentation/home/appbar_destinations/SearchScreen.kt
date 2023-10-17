5b4e5744aed7f2066102f8a9e106ea7f439b958e
sam <swwakoli@gmail.com>
2023-10-17 11:47:56 +0300

    implemented search ui and navigation

--- app/src/main/java/com/samwrotethecode/socialease/ui/presentation/home/appbar_destinations/SearchScreen.kt
+++ app/src/main/java/com/samwrotethecode/socialease/ui/presentation/home/appbar_destinations/SearchScreen.kt
@@ -1,6 +1,8 @@
 package com.samwrotethecode.socialease.ui.presentation.home.appbar_destinations
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,14 +11,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.SearchOff
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -26,91 +33,156 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.samwrotethecode.socialease.R
+import com.samwrotethecode.socialease.data.local_data.SubTopicsModel
+import com.samwrotethecode.socialease.ui.presentation.home.viewmodels.HomeScreenViewModel
 import com.samwrotethecode.socialease.ui.presentation.home.viewmodels.SearchScreenViewModel
+import com.samwrotethecode.socialease.ui.presentation.navigation.Screens
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen(
     navHostController: NavHostController,
-    viewModel: SearchScreenViewModel,
+    searchScreenViewModel: SearchScreenViewModel,
+    homeScreenViewModel: HomeScreenViewModel
 ) {
-    val uiState = viewModel.uiState.collectAsState().value
+    val uiState = searchScreenViewModel.uiState.collectAsState().value
     val context = LocalContext.current
 
-    Column(
-        modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
+    Box(
+        modifier = Modifier.fillMaxSize()
     ) {
-        Card(
-            modifier = Modifier.padding(16.dp)
-        ) {
-            TextField(value = uiState.query,
-                onValueChange = {
-                    viewModel.updateSearchQuery(it)
-                    viewModel.startSearch(context = context)
-                },
-                colors = TextFieldDefaults.colors(
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent,
-                ),
-                modifier = Modifier
-                    .align(alignment = Alignment.CenterHorizontally)
-                    .widthIn(max = 600.dp, min = 420.dp),
-                leadingIcon = {
-                    IconButton(onClick = { navHostController.navigateUp() }) {
-                        Icon(
-                            imageVector = Icons.Default.ArrowBack,
-                            contentDescription = stringResource(id = R.string.navigate_back)
-                        )
-                    }
-                },
-                placeholder = {
-                    Text(text = stringResource(id = R.string.search))
-                },
-                trailingIcon = {
-                    IconButton(onClick = { viewModel.startSearch(context = context) }) {
-                        Icon(imageVector = Icons.Outlined.Search, contentDescription = "Search")
-                    }
-                })
-        }
+        Image(
+            painter = painterResource(id = R.drawable.intro_img_1),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop,
+            alpha = 0.1f
+        )
         Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState()),
-            verticalArrangement = if (uiState.searchResults.isEmpty()) Arrangement.Center else Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            if (uiState.searchResults.isEmpty()) {
-                Icon(
-                    imageVector = Icons.Outlined.SearchOff,
-                    contentDescription = null,
-                    modifier = Modifier.size(64.dp),
-                    tint = MaterialTheme.colorScheme.primary,
-                )
-                Spacer(modifier = Modifier.height(20.dp))
-                Text(
-                    text = stringResource(R.string.no_results_found),
-                    style = TextStyle(
-                        fontSize = MaterialTheme.typography.headlineSmall.fontSize,
-                        fontWeight = MaterialTheme.typography.headlineSmall.fontWeight,
-                        color = MaterialTheme.colorScheme.primary
+            Card(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                TextField(
+                    value = uiState.query,
+                    onValueChange = {
+                        searchScreenViewModel.updateSearchQuery(it)
+                        searchScreenViewModel.startSearch(context = context)
+                    },
+                    colors = TextFieldDefaults.colors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
                     ),
+                    modifier = Modifier
+                        .align(alignment = Alignment.CenterHorizontally)
+                        .widthIn(max = 600.dp, min = 420.dp),
+                    leadingIcon = {
+                        IconButton(onClick = { navHostController.navigateUp() }) {
+                            Icon(
+                                imageVector = Icons.Default.ArrowBack,
+                                contentDescription = stringResource(id = R.string.navigate_back)
+                            )
+                        }
+                    },
+                    placeholder = {
+                        Text(text = stringResource(id = R.string.search))
+                    },
+                    trailingIcon = {
+                        IconButton(onClick = { searchScreenViewModel.startSearch(context = context) }) {
+                            Icon(imageVector = Icons.Outlined.Search, contentDescription = "Search")
+                        }
+                    },
+                    keyboardActions = KeyboardActions(
+                        onSearch = {
+                            searchScreenViewModel.startSearch(context = context)
+                        },
+                    ),
+                    keyboardOptions = KeyboardOptions(
+                        autoCorrect = true,
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Search
+                    )
                 )
-            } else {
-                for (item in uiState.searchResults) {
-                    Text(text = stringResource(id = item.titleId))
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = if (uiState.searchResults.isEmpty()) Arrangement.Center else Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                if (uiState.searchResults.isEmpty()) {
+                    Icon(
+                        imageVector = Icons.Outlined.SearchOff,
+                        contentDescription = null,
+                        modifier = Modifier.size(64.dp),
+                        tint = MaterialTheme.colorScheme.primary,
+                    )
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Text(
+                        text = stringResource(R.string.no_results_found),
+                        style = TextStyle(
+                            fontSize = MaterialTheme.typography.headlineSmall.fontSize,
+                            fontWeight = MaterialTheme.typography.headlineSmall.fontWeight,
+                            color = MaterialTheme.colorScheme.primary
+                        ),
+                    )
+                } else {
+                    for (subTopicsModelItem in uiState.searchResults) {
+                        Card(
+                            modifier = Modifier
+                                .padding(vertical = 4.dp, horizontal = 8.dp)
+                                .widthIn(max = 600.dp),
+                            onClick = {
+//                                Log.d(
+//                                    "SEARCH SCREEN",
+//                                    "CLICKED TITLE: ${
+//                                        getString(context, subTopicsModelItem.titleId)
+//                                    }"
+//                                )
+                                navHostController.navigate(Screens.ReadingScreen.route) {
+                                    launchSingleTop = true
+                                    popUpTo(Screens.SearchScreen.route) {
+                                        inclusive = true
+                                    }
+                                    homeScreenViewModel.updateReadingScreenState(
+                                        SubTopicsModel(
+                                            titleId = subTopicsModelItem.titleId,
+                                            generalDescriptionId = subTopicsModelItem.generalDescriptionId,
+                                            content = subTopicsModelItem.content,
+                                        )
+                                    )
+                                }
+                            },
+                            colors = CardDefaults.cardColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer
+                            ),
+                        ) {
+                            ListItem(headlineContent = {
+                                Text(
+                                    text = stringResource(id = subTopicsModelItem.titleId)
+                                )
+                            })
+                        }
+                    }
                 }
             }
         }
     }
-
 }
 
 @Preview(
@@ -121,6 +193,7 @@ fun SearchScreen(
 fun SearchScreenPreview() {
     SearchScreen(
         navHostController = rememberNavController(),
-        viewModel = viewModel<SearchScreenViewModel>(),
+        searchScreenViewModel = viewModel<SearchScreenViewModel>(),
+        homeScreenViewModel = viewModel<HomeScreenViewModel>()
     )
 }
