e8ea2d3e2ffb486f5d0e66dd87b846cf6c9fb364
sam <swwakoli@gmail.com>
2023-10-21 00:20:13 +0300

    refine d ReadingScreenDropdownMenu

--- app/src/main/java/com/samwrotethecode/socialease/ui/presentation/home/content/ReadingScreenDropdownMenu.kt
+++ app/src/main/java/com/samwrotethecode/socialease/ui/presentation/home/content/ReadingScreenDropdownMenu.kt
@@ -1,10 +1,7 @@
 package com.samwrotethecode.socialease.ui.presentation.home.content
 
 import android.widget.Toast
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.background
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Bookmark
 import androidx.compose.material.icons.outlined.BookmarkAdd
@@ -12,6 +9,8 @@ import androidx.compose.material.icons.outlined.Share
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -19,11 +18,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -51,26 +48,28 @@ fun ReadingScreenDropdownMenu(
 
     DropdownMenu(
         expanded = uiState.showDropdownMenu,
+        modifier = Modifier.background(MaterialTheme.colorScheme.secondaryContainer),
         onDismissRequest = { viewModel.updateAppbarDropDownMenu() }
     ) {
         DropdownMenuItem(
+            colors = MenuDefaults.itemColors(
+                leadingIconColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                textColor = MaterialTheme.colorScheme.onSecondaryContainer,
+            ),
+            leadingIcon = {
+                Icon(
+                    imageVector =
+                    if (isBookmarked) Icons.Outlined.Bookmark
+                    else Icons.Outlined.BookmarkAdd, contentDescription = null
+                )
+            },
             text = {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center,
-                ) {
-                    Icon(
-                        imageVector =
-                        if (isBookmarked) Icons.Outlined.Bookmark
-                        else Icons.Outlined.BookmarkAdd, contentDescription = null
-                    )
-                    Spacer(modifier = Modifier.width(12.dp))
-                    Text(
-                        text =
-                        if (isBookmarked) "Remove from bookmarks"
-                        else "Add to bookmarks"
-                    )
-                }
+                Text(
+                    text =
+                    if (isBookmarked) "Remove from bookmarks"
+                    else "Add to bookmarks"
+                )
+
             },
             onClick = {
                 viewModel.updateAppbarDropDownMenu()
@@ -90,20 +89,20 @@ fun ReadingScreenDropdownMenu(
             }
         )
         DropdownMenuItem(
+            colors = MenuDefaults.itemColors(
+                leadingIconColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                textColor = MaterialTheme.colorScheme.onSecondaryContainer,
+            ),
+            leadingIcon = {
+                Icon(
+                    imageVector =
+                    Icons.Outlined.Share, contentDescription = null
+                )
+            },
             text = {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center,
-                ) {
-                    Icon(
-                        imageVector =
-                        Icons.Outlined.Share, contentDescription = null
-                    )
-                    Spacer(modifier = Modifier.width(12.dp))
-                    Text(
-                        text = "Share"
-                    )
-                }
+                Text(
+                    text = "Share"
+                )
             },
             onClick = {
                 viewModel.updateAppbarDropDownMenu()
